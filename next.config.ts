c1363d011ffa9302cbec4e85414398bcb639ff08
jfd <xingli7986@gmail.com>
2026-05-12 14:38:21 +0800

    fix: broaden serverless bundle excludes

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ['sharp', '@prisma/client'],
+  serverExternalPackages: ['sharp', '@prisma/client', '@react-pdf/renderer', 'potrace'],
   outputFileTracingExcludes: {
     '*': [
-      './node_modules/@img/sharp-win32-*/**',
-      './node_modules/@img/sharp-darwin-*/**',
-      './node_modules/@img/sharp-libvips-win32-*/**',
-      './node_modules/@img/sharp-libvips-darwin-*/**',
-      './node_modules/@img/sharp-libvips-linuxmusl-arm64/**',
-      './node_modules/@img/sharp-linux-arm*/**',
-      './node_modules/@img/sharp-libvips-linux-arm*/**',
+      'node_modules/@img/sharp-win32-*',
+      'node_modules/@img/sharp-darwin-*',
+      'node_modules/@img/sharp-libvips-win32-*',
+      'node_modules/@img/sharp-libvips-darwin-*',
+      'node_modules/@img/sharp-libvips-linuxmusl-arm64',
+      'node_modules/@img/sharp-linux-arm*',
+      'node_modules/@img/sharp-libvips-linux-arm*',
+      'node_modules/prisma/libquery_engine-*',
+      'node_modules/@prisma/engines/**',
+      'node_modules/.prisma/client/libquery_engine-*',
+      '!node_modules/.prisma/client/libquery_engine-debian-openssl*',
+      'node_modules/@react-pdf/**',
+      'node_modules/potrace/**',
     ],
   },
   images: {
